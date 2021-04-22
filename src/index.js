f241d909e0647f89cdcdfff2e7b1ddf616e7cfa0
Dulatto <dulat.omarov@gmail.com>
2021-04-21 23:21:20 -0400

    action creator

--- src/index.js
+++ src/index.js
@@ -8,26 +8,27 @@ import { inc, dec, rnd } from './actions';
 const store = createStore(reducer);
 const [dispatch] = store;
 
+const bindActionCreator = (creator, dispatch) => (...args) => {
+  dispatch(creator(...args));
+}
 
-
+const incDispatch = bindActionCreator(inc, dispatch);
+const decDispatch = bindActionCreator(dec, dispatch);
+const rndDispatch = bindActionCreator(rnd, dispatch);
 
 document
   .getElementById('inc')
-  .addEventListener('click', () => {
-    dispatch(inc());
-  });
+  .addEventListener('click', incDispatch);
 
 document
   .getElementById('dec')
-  .addEventListener('click', () => {
-    dispatch(dec());
-  });
+  .addEventListener('click', decDispatch);
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = + Math.floor(Math.random() * 10);
-    dispatch(rnd(payload));
+    rndDispatch(payload);
   });
 
 
