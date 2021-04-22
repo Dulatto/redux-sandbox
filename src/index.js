2c7d2165be26b315a0cf8675d2b4bd61edf0fce9
Dulatto <dulat.omarov@gmail.com>
2021-04-21 23:13:23 -0400

    action creator

--- src/index.js
+++ src/index.js
@@ -1,47 +1,33 @@
 import { createStore } from 'redux';
+import reducer from './reducer';
+import { inc, dec, rnd } from './actions';
 
 
 
-const reducer = (state = 0, action) => {
-
-
-  switch (action.type) {
-    case 'RND':
-      return state = action.payload;
-    case 'INC':
-      return state + 1;
-    case 'DEC':
-      return state - 1;
-
-    default:
-      return state;
-  }
-};
 
 const store = createStore(reducer);
+const [dispatch] = store;
 
-const inc = () => ({ type: 'INC' });
-const dec = () => ({ type: 'DEC' });
-const rnd = (payload) => ({ type: 'RND', payload });
+
 
 
 document
   .getElementById('inc')
   .addEventListener('click', () => {
-    store.dispatch(inc());
+    dispatch(inc());
   });
 
 document
   .getElementById('dec')
   .addEventListener('click', () => {
-    store.dispatch(dec());
+    dispatch(dec());
   });
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = + Math.floor(Math.random() * 10);
-    store.dispatch(rnd(payload));
+    dispatch(rnd(payload));
   });
 
 
