3f594161edb922052a910c0d8729be965e538f3e
Dulatto <dulat.omarov@gmail.com>
2021-04-23 23:18:11 -0400

    react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/app';
 import reducer from './reducer';
 
-
 const store = createStore(reducer);
-
 
 ReactDOM.render(
   <Provider store={store}>
